0d28914aaf16c461e0f11e9fb18cbfbba0cf6042
Pherko <skateordieferi@gmail.com>
2024-09-25 20:43:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the element with the id "header"
+const header = document.getElementById("header");
+
+// Add an event listener to the header element
+header.addEventListener("mouseover", function() {
+  // Change the background color of the header on mouseover
+  header.style.backgroundColor = "#ccc";
+});
